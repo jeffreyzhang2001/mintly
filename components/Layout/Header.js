559e28f897c977611a55457e652cd68573ebc582
Jeffrey Zhang <jeffrey346@gmail.com>
2020-09-17 02:21:13 -0400

    Decrease header width for desktop

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -21,43 +21,45 @@ const Header = () => {
     return (
         <div>
             <header>
-                <Link href="/">
-                    <a>
-                        <Twemoji options={{ className: 'emoji' }}>🍃</Twemoji>
-                    </a>
-                </Link>
-                <div className="currentpage-text-container">
-                    <h1 className="slash">/</h1>
-                    <h1 className="currentpage">{currentPage}</h1>
+                <div className="header-container">
+                    <Link href="/">
+                        <a>
+                            <Twemoji options={{ className: 'emoji' }}>🍃</Twemoji>
+                        </a>
+                    </Link>
+                    <div className="currentpage-text-container">
+                        <h1 className="slash">/</h1>
+                        <h1 className="currentpage">{currentPage}</h1>
+                    </div>
+                    {user ? (
+                        <>
+                            <Link href="/profile">
+                                <h2 className="loginBtn">Profile</h2>
+                            </Link>
+                            <Link href="/dashboard">
+                                <Button type="primary" shape="round" size="large">
+                                    Dashboard
+                                </Button>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link href={'/login'}>
+                                <h2 className="loginBtn">Log In</h2>
+                            </Link>
+                            <Link href={'/register'}>
+                                <Button type="primary" shape="round" size="middle">
+                                    Sign Up
+                                </Button>
+                            </Link>
+                        </>
+                    )}
                 </div>
-                {user ? (
-                    <>
-                        <Link href="/profile">
-                            <h2 className="loginBtn">Profile</h2>
-                        </Link>
-                        <Link href="/dashboard">
-                            <Button type="primary" shape="round" size="large">
-                                Dashboard
-                            </Button>
-                        </Link>
-                    </>
-                ) : (
-                    <>
-                        <Link href={'/login'}>
-                            <h2 className="loginBtn">Log In</h2>
-                        </Link>
-                        <Link href={'/register'}>
-                            <Button type="primary" shape="round" size="middle">
-                                Sign Up
-                            </Button>
-                        </Link>
-                    </>
-                )}
             </header>
             <style jsx>{`
                 header {
                     display: flex;
-                    align-items: center;
+                    justify-content: center;
                     padding: 0 15px 0 15px;
                     background-color: #121212;
                     position: absolute;
@@ -65,6 +67,19 @@ const Header = () => {
                     width: 100%;
                     height: 70px;
                     overflow: hidden;
+                }
+
+                .header-container {
+                    display: flex;
+                    align-items: center;
+                    width: 70%;
+                    height: 100%;
+                }
+
+                @media only screen and (max-width: 600px) {
+                    .header-container {
+                        width: 100%;
+                    }
                 }
 
                 :global(.emoji) {
@@ -109,7 +124,6 @@ const Header = () => {
                 h1 {
                     margin-bottom: 0;
                     margin-left: 10px;
-                    font-weight: 400;
                     font-size: 25px;
                 }
 
