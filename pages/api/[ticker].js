54950f968db7f5d95d69dd05e29daca94c99d595
Jeffrey Zhang <jeffrey346@gmail.com>
2020-09-21 00:05:48 -0400

    Update [ticker] API route for invalid tickers

--- pages/api/[ticker].js
+++ pages/api/[ticker].js
@@ -6,19 +6,19 @@ export default (req, res) => {
     const finnhubClient = new finnhub.DefaultApi()
 
     finnhubClient.quote(req.query.ticker, (error, data, response) => {
-        if (data) {
-            const { o, h, l, c, pc } = data
+        const { o, h, l, c, pc } = data
+        if ((!o && !h && !l && !c && !pc) || error) {
+            return res.status(404).json({
+                status: 404,
+                message: 'Ticker not found',
+            })
+        } else {
             res.status(200).json({
                 open: o,
                 high: h,
                 low: l,
                 current: c,
                 prevClose: pc,
-            })
-        } else if (error) {
-            return res.status(404).json({
-                status: 404,
-                message: 'Ticker not found',
             })
         }
     })
