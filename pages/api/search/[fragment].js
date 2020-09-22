9f5e381393387c716d6252bfe96300d6e6f99e7d
Jeffrey Zhang <jeffrey346@gmail.com>
2020-09-22 17:14:19 -0400

    Improve ticker search

--- pages/api/search/[fragment].js
+++ pages/api/search/[fragment].js
@@ -6,9 +6,18 @@ export default async (req, res) => {
     } = req
     fragment = fragment.toLowerCase()
 
-    const matches = tickerList.filter(
-        (stockObj) =>
-            JSON.stringify(stockObj).toLowerCase().indexOf(fragment) !== -1,
-    )
+    let exactMatch
+    let exactMatchObj
+    let matches = tickerList.filter((stockObj) => {
+        if (stockObj.symbol.toLowerCase() === fragment) {
+            exactMatch = true
+            exactMatchObj = stockObj
+            return false
+        }
+        return JSON.stringify(stockObj).toLowerCase().indexOf(fragment) !== -1
+    })
+    if (exactMatch) {
+        matches.unshift(exactMatchObj)
+    }
     res.status(200).json({ matches })
 }
