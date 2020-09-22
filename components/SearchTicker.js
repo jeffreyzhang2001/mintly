3df6ebbb00c9cedfeafeaa47a1974678c8b7a61c
Jeffrey Zhang <jeffrey346@gmail.com>
2020-09-21 23:40:52 -0400

    Turn TickerSearch box from dashboard into component

---
+++ components/SearchTicker.js
@@ -0,0 +1,72 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+import { Button, AutoComplete } from 'antd'
+import axios from 'axios'
+
+const SearchTicker = ({ onSelect }) => {
+    const [autoCompleteValue, setAutoCompleteValue] = useState('')
+    const [autoCompleteData, setAutoCompleteData] = useState([])
+    const handleSearch = (text) => {
+        const renderLabel = (symbol, name) => {
+            return {
+                value: symbol,
+                label: (
+                    <div
+                        style={{
+                            display: 'flex',
+                        }}
+                    >
+                        <span style={{ fontWeight: 600 }}>{symbol}</span>
+                        <span style={{ marginLeft: 'auto' }}>{name}</span>
+                    </div>
+                ),
+            }
+        }
+
+        const getTickerFromAPI = async (text) => {
+            const res = await axios.get(`/api/search/${text}`)
+            const stocks = res.data?.matches?.map((stock) =>
+                renderLabel(stock.symbol, stock.name),
+            )
+            setAutoCompleteData(stocks)
+        }
+
+        if (text && text.match(/^[0-9a-zA-Z]+$/)) {
+            setAutoCompleteValue(text)
+            getTickerFromAPI(text)
+        } else {
+            setAutoCompleteValue(text)
+            setAutoCompleteData([])
+        }
+    }
+
+    return (
+        <>
+            <AutoComplete
+                className="autocomplete"
+                placeholder="Search Ticker (e.g. AAPL)"
+                options={autoCompleteData}
+                value={autoCompleteValue}
+                onChange={(text) => handleSearch(text)}
+                onSelect={(value) => {
+                    onSelect(value)
+                    setAutoCompleteData([])
+                }}
+            />
+            <style jsx>{`
+                :global(.autocomplete) {
+                    width: 100%;
+                    background: #cfd7ff;
+                    border: 1px solid #33ffaa;
+                    box-shadow: 3px 4px 0px #008f53;
+                }
+            `}</style>
+        </>
+    )
+}
+
+SearchTicker.propTypes = {
+    onSelect: PropTypes.func.isRequired,
+}
+
+export default SearchTicker
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,13 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-import axios from 'axios'
 import nookies from 'nookies'
 import { firebaseAdmin } from '../utils/firebase/firebaseAdmin'
-import { firebaseClient } from '../utils/firebase/firebaseClient'
 import useFirestore from '../utils/hooks/useFirestore'
 
-import { Button, AutoComplete } from 'antd'
+import { Button } from 'antd'
 import Skeleton from 'react-loading-skeleton'
+import SearchTicker from '../components/SearchTicker'
 
 const Dashboard = ({ uid }) => {
     const {
@@ -27,57 +26,12 @@ const Dashboard = ({ uid }) => {
     } = userInfo || {}
 
     const [selectedTicker, setSelectedTicker] = useState()
-    const [autoCompleteValue, setAutoCompleteValue] = useState('')
-    const [autoCompleteData, setAutoCompleteData] = useState([])
-    const handleSearch = (text) => {
-        const renderLabel = (symbol, name) => {
-            return {
-                value: symbol,
-                label: (
-                    <div
-                        style={{
-                            display: 'flex',
-                        }}
-                    >
-                        <span style={{ fontWeight: 600 }}>{symbol}</span>
-                        <span style={{ marginLeft: 'auto' }}>{name}</span>
-                    </div>
-                ),
-            }
-        }
-
-        const getTickerFromAPI = async (text) => {
-            const res = await axios.get(`/api/search/${text}`)
-            const stocks = res.data?.matches?.map((stock) =>
-                renderLabel(stock.symbol, stock.name),
-            )
-            setAutoCompleteData(stocks)
-        }
-
-        if (text && text.match(/^[0-9a-zA-Z]+$/)) {
-            setAutoCompleteValue(text)
-            getTickerFromAPI(text)
-        } else {
-            setAutoCompleteValue(text)
-            setAutoCompleteData([])
-        }
-    }
 
     return (
         <div className="dashboard">
             <main>
                 <div className="container">
-                    <AutoComplete
-                        className="autocomplete"
-                        placeholder="Search Ticker (e.g. AAPL)"
-                        options={autoCompleteData}
-                        value={autoCompleteValue}
-                        onChange={(text) => handleSearch(text)}
-                        onSelect={(value) => {
-                            setSelectedTicker(value)
-                            setAutoCompleteData([])
-                        }}
-                    />
+                    <SearchTicker onSelect={setSelectedTicker} />
                     <div className="portfolio-container">
                         {totalBalance || <Skeleton />}
                     </div>
@@ -103,13 +57,6 @@ const Dashboard = ({ uid }) => {
 
                 .container {
                     width: 55%;
-                }
-
-                :global(.autocomplete) {
-                    width: 100%;
-                    background: #cfd7ff;
-                    border: 1px solid #33ffaa;
-                    box-shadow: 3px 4px 0px #008f53;
                 }
 
                 .portfolio-container {
